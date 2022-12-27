65211adbeff2fe53d2faafdcf08b6811dd603fca
Param Yonzon <param.yonzon14@stjohns.edu>
2022-12-27 15:41:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("=== The Ultimate Wacky Recipe Maker ===")
+print("""Let's come up with a recipe generator to build us an amazing dish for today's evening meal!""")
+print()
+
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+method = input("Name a cooking method: ")
+burned = input("Name a word to describe burned food: ")
+item = input ("Name a househeld item: ")
+
+print(method, food, "with", burned, plant, "on a bed of", item)
